7afdbea56b069593afa31f60134c6f3fc2b92ace
Tomasz Łukasik <tomasz.lukasik@agora.pl>
2021-10-03 16:56:27 +0200

    added colors and theme

---
+++ lib/core/presentation/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color get dark => const Color(0xFF534C5F);
+  static Color get white => const Color(0xFFFFFFFF);
+  static Color get grey => const Color(0x29000000);
+  static Color get green => const Color(0xFF1DE278);
+}
---
+++ lib/core/presentation/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+final appTheme = ThemeData.from(
+  colorScheme: const ColorScheme.dark().copyWith(
+    background: AppColors.dark,
+  ),
+);
