2797c5585000063b31db169472709af110e4ea40
Tomasz Łukasik <tomasz.lukasik@agora.pl>
2021-10-04 22:48:25 +0200

    provide auth status notifier, auth repository

---
+++ lib/authentication/di/providers.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:applover/authentication/application/authentication_state_notifier.dart';
+import 'package:applover/authentication/data/authentication_repository_impl.dart';
+import 'package:applover/authentication/data/remote/authentication_remote_service.dart';
+import 'package:applover/authentication/domain/authentication_repository.dart';
+
+final authRemoteServiceProvider = Provider<AuthenticationRemoteService>(
+  (ref) => throw UnimplementedError(),
+);
+
+final authRepositoryProvider = Provider<AuthenticationRepository>(
+  (ref) => AuthenticationRepositoryImpl(ref.watch(authRemoteServiceProvider)),
+);
+
+final authenticationStateProvider = StateNotifierProvider<AuthenticationStatusNotifier, AuthenticationStatus>(
+  (ref) => AuthenticationStatusNotifier(ref.watch(authRepositoryProvider)),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:applover/core/presentation/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const AppWidget());
+  runApp(
+    const ProviderScope(
+      child: AppWidget(),
+    ),
+  );
 }
