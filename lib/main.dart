8cf5c813ed5a9ffdd5dc6899a9e629deb2a46d33
Tomasz Łukasik <tomasz.lukasik@agora.pl>
2021-10-04 22:50:15 +0200

    override auth remote dependency with fake service

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:applover/authentication/data/remote/fake_authentication_remote_service.dart';
+import 'package:applover/authentication/di/providers.dart';
 import 'package:applover/core/presentation/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: AppWidget(),
+    ProviderScope(
+      overrides: [
+        authRemoteServiceProvider.overrideWithValue(FakeAuthRemoteService()),
+      ],
+      child: const AppWidget(),
     ),
   );
 }
